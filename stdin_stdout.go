dbbff006fbc0910c3065e6f3295f18989a66c66a
Matheus Mina <mfbmina@gmail.com>
2021-03-30 09:37:56 +0200

    Add information about stdin and stdout

---
+++ stdin_stdout.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+// Sometimes we need to get or send information from the users.
+// In basic programs like ours, we do that by reading information from the terminal and printing it back there.
+// This is what we call "reading from the `stdin`" and "printing to the `stdout`"
+func main() {
+  var name string
+
+  // We are printing a question to the stdout
+  fmt.Println("What is your name?")
+
+  // We are reading the awnser from the user and storing it in a variable called name.
+  fmt.Scanln(&name)
+
+  fmt.Println("Hello", name)
+}
