541829e81c8f890518b0055898d3396de149c5e1
Matheus Mina <mfbmina@gmail.com>
2022-01-06 20:49:39 -0300

    Add fib example

--- recursion/basic.go
+++ recursion/basic.go
@@ -10,14 +10,27 @@ func Basic(n int64) {
 	fmt.Println("Calculating fatorial for", n)
 	resp := fat(n)
 	fmt.Println("The response is", resp)
+	fmt.Println("Calculating fibonacci for", n)
+	resp = fib(n)
+	fmt.Println("The response is", resp)
 }
 
-func fat(n int64) int64{
+func fat(n int64) int64 {
 	// stop condition
 	if n == 1 {
 		return 1
 	}
 
 	// calling itself again!
-	return n * fat(n - 1)
+	return n * fat(n-1)
+}
+
+func fib(n int64) int64 {
+	// stop condition
+	if n > 1 {
+		// calling itself again!
+		return fib(n-1) + fib(n-2)
+	}
+
+	return 1
 }
